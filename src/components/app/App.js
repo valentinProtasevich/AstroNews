8e382e5a7aa924f4d88f13dc9b42f4edaeba0cc7
Valentin <veksel_or_vels@mail.ru>
2022-08-08 17:01:00 +0300

    create form validation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,13 @@
 import AppHeader from "../appHeader/AppHeader";
+import RegistrationForm from "../registrationForm/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       <AppHeader/>
+      <main>
+        <RegistrationForm/>
+      </main>
     </div>
   );
 }

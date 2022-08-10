266b94e569d0a2b3713df7cc2818e847a4894017
Valentin <veksel_or_vels@mail.ru>
2022-08-10 11:49:32 +0300

    add firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './components/app/App';
+import './firebase';
 
 import './style/style.scss';
 

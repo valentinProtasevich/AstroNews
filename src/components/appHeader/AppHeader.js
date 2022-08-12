fbea6f11927a0d041c0e8ddb6571fe917470c242
Valentin <veksel_or_vels@mail.ru>
2022-08-12 15:29:47 +0300

    create signOut button and create login page

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,8 +1,51 @@
 import { NavLink, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import {useAuth} from '../../hooks/use-auth';
+import { removeUser } from '../../store/slices/userSlice';
 
 import './appHeader.scss';
 
 const AppHeader = () => {
+  const dispatch = useDispatch();
+
+  const {isAuth} = useAuth();
+
+  const accountButtons = () => {
+    if (isAuth) {
+      return (
+        <>
+          <li><NavLink 
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
+              to='/account'>Личный кабинет</NavLink>
+          </li>
+          <li><NavLink
+              onClick={() => dispatch(removeUser())} 
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
+              to='/login'>Выйти</NavLink>
+          </li>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <li><NavLink 
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
+              to='/login'>Войти</NavLink>
+          </li>
+          <li><NavLink 
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
+              to='/registration'>Регистрация</NavLink>
+          </li>
+        </>
+      )
+    }
+  };
+  
   return (
     <header className='app__header'>
       <h1 className='app__title'>
@@ -15,22 +58,12 @@ const AppHeader = () => {
               className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
               to=''>Главная</NavLink>
           </li>
-          <li><a href="">Статьи</a></li>
           <li><NavLink 
               end 
               className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
-              to='/account'>Личный кабинет</NavLink>
+              to='/articles'>Статьи</NavLink>
           </li>
-          <li><NavLink 
-              end 
-              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
-              to='/login'>Вход</NavLink>
-          </li>
-          <li><NavLink 
-              end 
-              className={({ isActive }) => "" + (isActive ? " app__menu_selected" : "")}
-              to='/registration'>Регистрация</NavLink>
-          </li>
+          {accountButtons()}
         </ul>
       </nav>
     </header>

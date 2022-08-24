f36f0d95a10b145fca623916ea9cae8bf2469825
Valentin <veksel_or_vels@mail.ru>
2022-08-24 10:29:18 +0300

    create slider with news

--- src/components/pages/homePage/Homepage.js
+++ src/components/pages/homePage/Homepage.js
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet";
 import { Link } from 'react-router-dom';
 
+import { useGetNewsQuery } from "../../../api/newsApi";
+
 import SimpleSlider from "../../simpleSlider/SimpleSlider";
 
 import './homePage.scss';
@@ -8,6 +10,19 @@ import './homePage.scss';
 import bg_planet from '../../../resources/img/home-img.jpg';
 
 const Homepage = () => {
+  const {data = []} = useGetNewsQuery();
+
+  let getDate = (date) => {
+    let dateObj = new Date(Date.parse(date));
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth() + 1;
+    if (month < 10) {
+      month = '0' + month;
+    };
+    let day = dateObj.getDate();
+    return `${day}.${month}.${year}`;
+  };
+  
   return (
     <>
       <Helmet>
@@ -23,8 +38,19 @@ const Homepage = () => {
           <h1>AstroNews — <br /> портал о космосе</h1>
           <p>Статьи о космосе и космических объектах</p>
           <Link to='/articles'>Статьи</Link>
-          {/* <SimpleSlider/> */}
         </article>
+        <SimpleSlider>
+          {data.map(item => (
+            <div id={item.id} className="homePage__newsContainer">
+              <img src={item.imageUrl} alt="" />
+              <a href={item.url}>{item.title}</a>
+              <div className="homePage__newsContainer_newsSite">
+                <p>{item.newsSite} / </p>
+                <p>{getDate(item.publishedAt)}</p>
+              </div>
+            </div>
+          ))}
+        </SimpleSlider>
       </div>
     </>
   )
--- src/components/simpleSlider/SimpleSlider.js
+++ src/components/simpleSlider/SimpleSlider.js
@@ -1,36 +1,22 @@
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const SimpleSlider = () => {
+const SimpleSlider = (props) => {
   const settings = {
-    dots: true,
-    infinite: true,
+    className: '',
+    dots: false,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
   };
 
   return (
-    <div>
-        <h2> Single Item</h2>
+    <div className="slider">
+        <h2>Статьи</h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {props.children}
         </Slider>
       </div>
   )
